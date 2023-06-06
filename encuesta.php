867b96eb7839310da5b225c2cb6b6c499d623b70
MasterMindGG <hackathongrupo@gmail.com>
2023-06-06 12:58:39 -0400

    registrare.php incluido

--- encuesta.php
+++ encuesta.php
@@ -5,12 +5,14 @@ include('conexion.php');
 
 $option = null;
 $pais = 'Paraguay';
+
 # $pais = $_REQUEST['pais'];
+
 if($pais == 'Paraguay' or $pais == 'Argentina' or $pais == 'Uruguay')
 {
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,17 +24,36 @@ if($pais == 'Paraguay' or $pais == 'Argentina' or $pais == 'Uruguay')
 </head>
 <body>
 
-<h1>Choose a book format</h1>
+<h1>Encuesta hackathon</h1>
+<h2>Generales</h2>
+<section>seccion 1</section>
+<form action="registrare.php" method="POST">
+
+<label for="">Pais</label>
+<input type="text" name="pais" value="<?php echo $pais; ?>">
+<br>
+
+<label for="">Edad</label>
+<input type="number" name="edad" min="12" max="99">
+<br>
+
+<p>Genero</p>
+<input type="radio" name="genero" value="Masculino"> Masculino
+<input type="radio" name="genero" value="Femenino"> Femenino
+<input type="radio" name="genero" value="Otro"> Otro
+
+
 <div class="select">
    <select name="format" id="format">
-      <option selected disabled>Choose a book format</option>
-      <option value="pdf">PDF</option>
-      <option value="txt">txt</option>
-      <option value="epub">ePub</option>
+      <option selected disabled>Pais</option>
+      <option value="pdf">Argewtina</option>
+      <option value="txt">Brasil</option>
+      <option value="epub"></option>
       <option value="fb2">fb2</option>
       <option value="mobi">mobi</option>
    </select>
 </div>
+</form>
 
 </body>
 </html>
---
+++ registrare.php
@@ -0,0 +1,40 @@
+<?php
+
+include('conexion.php');
+
+$option = null;
+$pais = 'Paraguay';
+
+$pais = $_REQUEST['pais'];
+$edad = $_REQUEST['edad'];
+$genero = $_REQUEST['genero'];
+$departamento = $_REQUEST['departamento'];
+$email = $_REQUEST['email'];
+
+$bachiller = $_REQUEST['bachiller'];
+$pasantias = $_REQUEST['pasantias'];
+$cargahr = $_REQUEST['cargahr'];
+$contenidos = $_REQUEST['contenidos'];
+
+$funcioncentro = $_REQUEST['funcioncentro'];
+$relalumnos = $_REQUEST['relalumnos'];
+$reldocentes = $_REQUEST['reldocentes'];
+$bullying = $_REQUEST['bullying'];
+
+$estadoinstitucional = $_REQUEST['estadoinstitucional'];
+$wifi = $_REQUEST['wifi'];
+$cantequipamientos = $_REQUEST['cantequipamentos'];
+$calequipamientos = $_REQUEST['calequipamentos'];
+$ace = $_REQUEST['ace'];
+$muebles = $_REQUEST['muebles'];
+$matespecificas = $_REQUEST['matespecificas'];
+
+
+
+
+
+
+
+
+
+?>
